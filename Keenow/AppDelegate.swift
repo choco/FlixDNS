1d3bf759091cdb6619a982ae0fef5bce9e4a3c6a
Enrico Ghirardi <i@choco.me>
2017-04-28 16:17:13 +0200

    Initial Commit

---
+++ Keenow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Keenow
+//
+//  Created by Enrico Ghirardi on 28/04/2017.
+//  Copyright © 2017 Enrico Ghirardi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeenowTests/KeenowTests.swift
@@ -0,0 +1,36 @@
+//
+//  KeenowTests.swift
+//  KeenowTests
+//
+//  Created by Enrico Ghirardi on 28/04/2017.
+//  Copyright © 2017 Enrico Ghirardi. All rights reserved.
+//
+
+import XCTest
+@testable import Keenow
+
+class KeenowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
